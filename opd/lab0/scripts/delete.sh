35be5ed5eb40b0844828ce3c4a5da0970b81d842
Egor Fedorov <fegor2004@gmail.com>
2022-09-10 15:46:13 +0300

    Добавлен вывод скрипта

--- opd/lab0/scripts/delete.sh
+++ opd/lab0/scripts/delete.sh
@@ -1,7 +1,7 @@
-rm karrablast9 -f
-rm accelgor3/roserade/snubbull -f
-rm Copy_* -f
-rm golem8/nidorino/chingling/haxorusmacho* -f
-rm mothim1/* -f
+rm -f karrablast9
+rm -f accelgor3/roserade/snubbull
+rm -f Copy_*
+rm -f golem8/nidorino/chingling/haxorusmacho*
+rm -f mothim1/*
 rmdir mothim1
 rm -rf acelgor3/lampent/mismagius/emboar
