b89f98cfb79f6f6c4adbc41cc13d97fb75fa456b
Egor Fedorov <fegor2004@gmail.com>
2023-09-05 10:31:07 +0300

    Добавлен placeholder для input

--- web/lab1/deploy.sh
+++ web/lab1/deploy.sh
@@ -1,4 +1,6 @@
 pnpm run build
 sed -i 's/\/assets/\.\/assets/g' dist/index.html
 sed -i "s/GIT_VERSION/$(git rev-parse --short HEAD)/g" dist/index.html
+echo 'Uploading dist and php to helios'
 rsync -r {dist,php}/* helios:~/public_html
+echo 'FINISHED'
